1858522126ef608ec8595421516d9ac0e537bf05
Uros Tement <utement@gmail.com>
2013-03-10 21:25:04 +0100

    Working kommunication with Paymill.
    Can send request and get token as result.

--- CRM/Core/Payment/Paymill.php
+++ CRM/Core/Payment/Paymill.php
@@ -32,7 +32,7 @@ class CRM_Core_Payment_Paymill extends CRM_Core_Payment {
     function __construct($mode, &$paymentProcessor) {
         $this->_mode = $mode;
         $this->_paymentProcessor = $paymentProcessor;
-        $this->_processorName = ts('Paymill Payment Collection');
+        $this->_processorName = ts('Paymill');
     }
 
     /**
@@ -63,11 +63,11 @@ class CRM_Core_Payment_Paymill extends CRM_Core_Payment {
         $error = array();
 
         if (empty($this->_paymentProcessor['user_name'])) {
-            $error[] = ts('The "Secret Key" is not set in the Stripe Payment Processor settings.');
+            $error[] = ts('The "Secret Key" is not set in the Paymill Payment Processor settings.');
         }
 
         if (empty($this->_paymentProcessor['password'])) {
-            $error[] = ts('The "Publishable Key" is not set in the Stripe Payment Processor settings.');
+            $error[] = ts('The "Publishable Key" is not set in the Paymill Payment Processor settings.');
         }
 
         if (!empty($error)) {
@@ -82,6 +82,39 @@ class CRM_Core_Payment_Paymill extends CRM_Core_Payment {
         if (empty($params['amount']) || $params['amount'] == 0) {
             return $params;
         }
+
+
+        // Paymill amount required in cents.
+        $amount = $params['amount'] * 100;
+        // It would require 3 digits after the decimal for one to make it this far.
+        // CiviCRM prevents this, but let's be redundant.
+        $amount = number_format($amount, 0, '', '');
+
+        // Include Paymill library & Set API credentials.
+        require_once("Paymill-PHP/lib/Services/Paymill/Transactions.php");
+
+
+        $transactionsObject = new Services_Paymill_Transactions($this->_paymentProcessor['user_name'], $this->_paymentProcessor['url_site']);
+
+
+        echo "<br><br>";
+
+
+     
+        
+        if (isset($params['paymill_token'])) {
+            $card_details = $params['paymill_token'];
+            if ($params['paymill_token'] == null) {
+                CRM_Core_Error::fatal(ts('Paymill token is NULL!'));
+            }
+        } else {
+            CRM_Core_Error::fatal(ts('Stripe.js token was not passed!'));
+        }
+        
+        
+        
+        
+        
     }
 
     /**
@@ -95,6 +128,97 @@ class CRM_Core_Payment_Paymill extends CRM_Core_Payment {
      */
     function doTransferCheckout(&$params, $component) {
         CRM_Core_Error::fatal(ts('This function is not implemented'));
+    }
+
+    /**
+     * Run Paymill calls through this to catch exceptions gracefully.
+     * @param  string $op
+     *   Determine which operation to perform.
+     * @param  array $params
+     *   Parameters to run Paymill calls on.
+     * @return varies
+     *   Response from gateway.
+     */
+    function paymillCatchErrors($op = 'create_customer', &$params, $qfKey = '') {
+        // @TODO:  Handle all calls through this using $op switching for sanity.
+        // Check for errors before trying to submit.
+        try {
+            switch ($op) {
+                case 'create_customer':
+                    $return = Stripe_Customer::create($params);
+                    break;
+
+                case 'charge':
+                    $return = Stripe_Charge::create($params);
+                    break;
+
+                case 'save':
+                    $return = $params->save();
+                    break;
+
+                case 'create_plan':
+                    $return = Stripe_Plan::create($params);
+                    break;
+
+                default:
+                    $return = Stripe_Customer::create($params);
+                    break;
+            }
+        } catch (Stripe_CardError $e) {
+            $error_message = '';
+            // Since it's a decline, Stripe_CardError will be caught
+            $body = $e->getJsonBody();
+            $err = $body['error'];
+
+            //$error_message .= 'Status is: ' . $e->getHttpStatus() . "<br />";
+            ////$error_message .= 'Param is: ' . $err['param'] . "<br />";
+            $error_message .= 'Type: ' . $err['type'] . "<br />";
+            $error_message .= 'Code: ' . $err['code'] . "<br />";
+            $error_message .= 'Message: ' . $err['message'] . "<br />";
+
+            // Check Event vs Contribution for redirect.  There must be a better way.
+            if (empty($params['selectMembership']) && empty($params['contributionPageID'])) {
+                $error_url = CRM_Utils_System::url('civicrm/event/register', "_qf_Main_display=1&cancel=1&qfKey=$qfKey", FALSE, NULL, FALSE);
+            } else {
+                $error_url = CRM_Utils_System::url('civicrm/contribute/transact', "_qf_Main_display=1&cancel=1&qfKey=$qfKey", FALSE, NULL, FALSE);
+            }
+
+            CRM_Core_Error::statusBounce("Oops!  Looks like there was an error.  Payment Response:
+        <br /> $error_message", $error_url);
+        } catch (Stripe_InvalidRequestError $e) {
+            // Invalid parameters were supplied to Paymill's API
+        } catch (Stripe_AuthenticationError $e) {
+            // Authentication with Paymill's API failed
+            // (maybe you changed API keys recently)
+        } catch (Stripe_ApiConnectionError $e) {
+            // Network communication with Paymill failed
+        } catch (Stripe_Error $e) {
+            // Display a very generic error to the user, and maybe send
+            // yourself an email
+        } catch (Exception $e) {
+            // Something else happened, completely unrelated to Paymill
+            $error_message = '';
+            // Since it's a decline, Stripe_CardError will be caught
+            $body = $e->getJsonBody();
+            $err = $body['error'];
+
+            //$error_message .= 'Status is: ' . $e->getHttpStatus() . "<br />";
+            ////$error_message .= 'Param is: ' . $err['param'] . "<br />";
+            $error_message .= 'Type: ' . $err['type'] . "<br />";
+            $error_message .= 'Code: ' . $err['code'] . "<br />";
+            $error_message .= 'Message: ' . $err['message'] . "<br />";
+
+            if (empty($params['selectMembership']) && empty($params['contributionPageID'])) {
+                $error_url = CRM_Utils_System::url('civicrm/event/register', "_qf_Main_display=1&cancel=1&qfKey=$qfKey", FALSE, NULL, FALSE);
+            } else {
+                $error_url = CRM_Utils_System::url('civicrm/contribute/transact', "_qf_Main_display=1&cancel=1&qfKey=$qfKey", FALSE, NULL, FALSE);
+            }
+
+            CRM_Core_Error::statusBounce("Oops!  Looks like there was an error.  Payment Response:
+        <br /> $error_message", $error_url);
+        }
+
+        return $return;
     }
 
 }
--- paymill.php
+++ paymill.php
@@ -2,11 +2,30 @@
 
 require_once 'paymill.civix.php';
 
+
+
+/**
+ * Implementation of hook_civicrm_buildForm().
+ * @param $formName - the name of the form
+ * @param $form - reference to the form object
+ */
+function paymill_civicrm_buildForm($formName, &$form) {
+  if(isset($form->_paymentProcessor['payment_processor_type'])
+    && $form->_paymentProcessor['payment_processor_type'] == 'Paymill') {
+    if(!stristr($formName, '_Confirm') && !stristr($formName, '_ThankYou')) {
+      if(!isset($form->_elementIndex['paymill_token'])) {
+        $form->addElement('hidden', 'paymill_token', NULL, array('id'=> 'paymill-token'));
+      }
+    }
+  }
+}
+
+
 /**
  * Implementation of hook_civicrm_config
  */
 function paymill_civicrm_config(&$config) {
-  _paymill_civix_civicrm_config($config);
+    _paymill_civix_civicrm_config($config);
 }
 
 /**
@@ -15,35 +34,40 @@ function paymill_civicrm_config(&$config) {
  * @param $files array(string)
  */
 function paymill_civicrm_xmlMenu(&$files) {
-  _paymill_civix_civicrm_xmlMenu($files);
+    _paymill_civix_civicrm_xmlMenu($files);
 }
 
 /**
  * Implementation of hook_civicrm_install
  */
 function paymill_civicrm_install() {
-  return _paymill_civix_civicrm_install();
+    CRM_Core_Session::setStatus("Inštaliram");
+
+    return _paymill_civix_civicrm_install();
 }
 
 /**
  * Implementation of hook_civicrm_uninstall
  */
 function paymill_civicrm_uninstall() {
-  return _paymill_civix_civicrm_uninstall();
+    CRM_Core_Session::setStatus("DEInštaliram");
+    return _paymill_civix_civicrm_uninstall();
 }
 
 /**
  * Implementation of hook_civicrm_enable
  */
 function paymill_civicrm_enable() {
-  return _paymill_civix_civicrm_enable();
+    CRM_Core_Session::setStatus("Omogočam");
+    return _paymill_civix_civicrm_enable();
 }
 
 /**
  * Implementation of hook_civicrm_disable
  */
 function paymill_civicrm_disable() {
-  return _paymill_civix_civicrm_disable();
+    CRM_Core_Session::setStatus("Onemogočam");
+    return _paymill_civix_civicrm_disable();
 }
 
 /**
@@ -56,9 +80,8 @@ function paymill_civicrm_disable() {
  *                for 'enqueue', returns void
  */
 function paymill_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
-  return _paymill_civix_civicrm_upgrade($op, $queue);
+    return _paymill_civix_civicrm_upgrade($op, $queue);
 }
-
 
 /**
  * Implementation of hook_civicrm_managed
@@ -67,26 +90,26 @@ function paymill_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
  * is installed, disabled, uninstalled.
  */
 function paymill_civicrm_managed(&$entities) {
-  $entities[] = array(
-  	'module' => 'net.utement.civi.payment.paymill',
-  	'name' => 'Paymill',
-  	'entity' => 'PaymentProcessorType',
-  	'params' => array(
-  	  'version' => 3,
-      'name' => 'Paymill',
-      'title' => 'Paymill',
-      'description' => 'Paymill Payment Processor',
-      'class_name' => 'Payment_Paymill',
-      'billing_mode' => 'form',
-      'user_name_label' => 'Secret Key',
-      'password_label' => 'Publishable Key',
-  	  'url_site_default'=> 'https://api.stripe.com/v1',
-      'url_recur_default' => 'https://api.stripe.com/v1',
-      'url_site_test_default' => 'https://api.stripe.com/v1',
-      'url_recur_test_default' => 'https://api.stripe.com/v1',
-      'is_recur' => 1,
-      'payment_type' => 1
-    ),
-  );
-  return _paymill_civix_civicrm_managed($entities);
+    $entities[] = array(
+        'module' => 'net.utement.civi.payment.paymill',
+        'name' => 'Paymill',
+        'entity' => 'PaymentProcessorType',
+        'params' => array(
+            'version' => 3,
+            'name' => 'Paymill',
+            'title' => 'Paymill',
+            'description' => 'Paymill Payment Processor',
+            'class_name' => 'Payment_Paymill',
+            'billing_mode' => 'form',
+            'user_name_label' => 'Secret Key',
+            'password_label' => 'Publishable Key',
+            'url_site_default' => 'https://api.paymill.com/v2/',
+            'url_recur_default' => 'https://api.paymill.com/v2/',
+            'url_site_test_default' => 'https://api.paymill.com/v2/',
+            'url_recur_test_default' => 'https://api.paymill.com/v2/',
+            'is_recur' => 1,
+            'payment_type' => 1
+        ),
+    );
+    return _paymill_civix_civicrm_managed($entities);
 }
